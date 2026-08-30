0fd2adfb2235e36ff1a0d1a305cda76a4838baa1
LeFractiste <lefractiste@outlook.fr>
2026-08-30 12:43:49 +0200

    fix(Mandel-V2): Add package to Mandel-V2 too !

---
+++ Mandel-V2/pkg/mandelbrot_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const mandelbrot_iter: (a: number, b: number, c: number) => number;
+export const render_mandelbrot: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
